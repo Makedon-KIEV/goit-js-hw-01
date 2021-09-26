52b3508472a85078c2d86968d8f8ddc99ff8a879
Alexandr <43139323+Makedon-KIEV@users.noreply.github.com>
2021-09-26 20:31:11 +0300

    1
    1

--- js/script-99.js
+++ js/script-99.js
@@ -1,3 +1,11 @@
-const greet = () => {
-  console.log("Привет!");
-};
+const numbers = [5, 10, 15, 20, 25];
+
+// Объявление функции
+numbers.forEach(function (number, index) {
+  console.log(`Индекс ${index}, значение ${number}`);
+});
+
+// Анонимная стрелочная функция
+numbers.forEach((number, index) => {
+  console.log(`Индекс ${index}, значение ${number}`);
+});
