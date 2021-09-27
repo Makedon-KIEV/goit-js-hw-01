8a6a8b2de11af08f1aea97cd480dd658f771b141
Alexandr <43139323+Makedon-KIEV@users.noreply.github.com>
2021-09-27 15:23:57 +0300

    1
    1

--- js/script-99.js
+++ js/script-99.js
@@ -1,4 +1,17 @@
-// Декларативный подход
+const pureMultiply = (array, value) => {
+  const newArray = [];
+
+  array.forEach(element => {
+    newArray.push(element * value);
+  });
+
+  return newArray;
+};
+
 const numbers = [1, 2, 3, 4, 5];
-const filteredNumbers = numbers.filter(value => value > 3);
-console.log(filteredNumbers); // [4, 5]
+const doubledNumbers = pureMultiply(numbers, 2);
+
+// Не произошло мутации исходных данных
+console.log(numbers); // [1, 2, 3, 4, 5]
+// Функция вернула новый массив с изменёнными данными
+console.log(doubledNumbers); // [2, 4, 6, 8, 10]
