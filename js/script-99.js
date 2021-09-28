0a4ec91f810442a597df953b8630161a2168508c
Alexandr <43139323+Makedon-KIEV@users.noreply.github.com>
2021-09-28 23:30:08 +0300

    1
    1

--- js/script-99.js
+++ js/script-99.js
@@ -4,3 +4,13 @@ const students = [
   { name: "Аякс", score: 37, courses: ["физика", "биология"] },
   { name: "Киви", score: 94, courses: ["литература", "информатика"] },
 ];
+const sortedByAscendingScore = [...students].sort((a, b) => a.score - b.score);
+const names = sortedByAscendingScore.map(student => student.name);
+
+console.log(names); // ['Аякс', 'Поли', 'Манго', 'Киви']
+
+const names = [...students]
+  .sort((a, b) => a.score - b.score)
+  .map(student => student.name);
+
+console.log(names); // ['Аякс', 'Поли', 'Манго', 'Киви']
