9f7e04fb7fba80ce399cde3b4f1808d6a048a14a
Alexandr <43139323+Makedon-KIEV@users.noreply.github.com>
2021-09-28 23:07:41 +0300

    1
    1

--- js/script-99.js
+++ js/script-99.js
@@ -1,14 +1,29 @@
-const students = [
-  { name: "Манго", score: 83 },
-  { name: "Поли", score: 59 },
-  { name: "Аякс", score: 37 },
-  { name: "Киви", score: 94 },
-  { name: "Хьюстон", score: 64 },
+const tweets = [
+  { id: "000", likes: 5, tags: ["js", "nodejs"] },
+  { id: "001", likes: 2, tags: ["html", "css"] },
+  { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
+  { id: "003", likes: 8, tags: ["css", "react"] },
+  { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
 ];
 
-// Название аккумулятора может быть произвольным, это просто параметр функции
-const totalScore = students.reduce((total, student) => {
-  return total + student.score;
-}, 0);
+// Пройдем по всем элементам коллекции и добавим значения свойства tags
+// к аккумулятору, начальное значение которого укажем пустым массивом [].
+// На каждой итерации пушим в аккумулятор все элементы tweet.tags и возвращаем его.
+const tags = tweets.reduce((allTags, tweet) => {
+  allTags.push(...tweet.tags);
 
-const averageScore = totalScore / students.length;
+  return allTags;
+}, []);
+
+console.log(tags);
+
+// Наверное сбор тегов не одиночная операция, поэтому напишем функцию
+// для сбора тегов из коллекции
+const getTags = tweets =>
+  tweets.reduce((allTags, tweet) => {
+    allTags.push(...tweet.tags);
+
+    return allTags;
+  }, []);
+
+console.log(getTags(tweets));
