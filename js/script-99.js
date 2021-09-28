7e588c1a1cae0aa1dfb3dc83ae0ed87f8a49bd93
Alexandr <43139323+Makedon-KIEV@users.noreply.github.com>
2021-09-28 23:30:59 +0300

    12
    12

--- js/script-99.js
+++ js/script-99.js
@@ -4,13 +4,20 @@ const students = [
   { name: "Аякс", score: 37, courses: ["физика", "биология"] },
   { name: "Киви", score: 94, courses: ["литература", "информатика"] },
 ];
-const sortedByAscendingScore = [...students].sort((a, b) => a.score - b.score);
-const names = sortedByAscendingScore.map(student => student.name);
+// const sortedByAscendingScore = [...students].sort((a, b) => a.score - b.score);
+// const names = sortedByAscendingScore.map(student => student.name);
 
-console.log(names); // ['Аякс', 'Поли', 'Манго', 'Киви']
+// console.log(names); // ['Аякс', 'Поли', 'Манго', 'Киви']
 
 const names = [...students]
   .sort((a, b) => a.score - b.score)
   .map(student => student.name);
 
 console.log(names); // ['Аякс', 'Поли', 'Манго', 'Киви']
+
+const uniqueSortedCourses = students
+  .flatMap(student => student.courses)
+  .filter((course, index, array) => array.indexOf(course) === index)
+  .sort((a, b) => a.localeCompare(b));
+
+console.log(uniqueSortedCourses); // ['биология', 'информатика', 'литература', 'математика', 'физика']
