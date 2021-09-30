5ce4a67d6daed3b33a2fad22f657caee86182347
Alexandr <43139323+Makedon-KIEV@users.noreply.github.com>
2021-09-30 19:13:36 +0300

    1
    1

--- js/script-99.js
+++ js/script-99.js
@@ -1,7 +1,19 @@
-const animal = { eats: true };
-const dog = Object.create(animal);
-dog.barks = true;
-
-for (const key in dog) {
-  console.log(key); // barks, eats
+class User {
+  // Деструктуризируем объект
+  constructor({ name, email }) {
+    this.name = name;
+    this.email = email;
+  }
 }
+
+const mango = new User({
+  name: "Манго",
+  email: "mango@mail.com",
+});
+console.log(mango); // { name: "Манго", email: "mango@mail.com" }
+
+const poly = new User({
+  name: "Поли",
+  email: "poly@mail.com",
+});
+console.log(poly); // { name: "Поли", email: "poly@mail.com" }
